03585ed35417fb64c08758ebb9ed2ee1bd2c3c32
Cruz Caine <cruz@cruzcaine.com>
2025-10-05 19:37:56 -0500

    Removed Redundant Code

--- src/finman/main.py
+++ src/finman/main.py
@@ -1,17 +1,10 @@
 import curses # imports curses a barebones highly portable tui library
 from finman.ui.main_menu import MainMenu
+import argparse
 
 def main():
     screen = curses.initscr() # creates the screen object we will be working with
-    curses.noecho() # stops key presses from being shown on the screen by default
-    curses.cbreak() # get key presses without the user pressing enter
-    screen.keypad(True) # get special values instead of multi-key sequences for certain key presses
-    screen.nodelay(True) # make getchr non-blocking
-    curses.curs_set(0) # disable the cursor
-    curses.start_color() # enable the usage of colors
-    num_rows, num_cols = screen.getmaxyx() 
-    menu_window = curses.newwin(1, 1, 3, 0)
-    title_window = curses.newwin(1, 1, 0, 0)
+    curses_init()
     
     inpt = screen.getch()
     main_menu = MainMenu(screen)
@@ -27,7 +20,19 @@ def main():
         curses.napms(100)
         # check for key presses
         inpt = screen.getch()
+
+    curses_exit()
     
+
+def curses_init():
+    curses.noecho() # stops key presses from being shown on the screen by default
+    curses.cbreak() # get key presses without the user pressing enter
+    screen.keypad(True) # get special values instead of multi-key sequences for certain key presses
+    screen.nodelay(True) # make getchr non-blocking
+    curses.curs_set(0) # disable the cursor
+    curses.start_color() # enable the usage of colors
+
+def curses_exit():
     # Undoes all the changes we made to the terminal during initialization
     curses.nocbreak()
     screen.keypad(False)
