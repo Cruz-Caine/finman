c22b97926320e7dd4a3c372ad460bd4fd04f69df
Cruz Caine <cruz@cruzcaine.com>
2025-10-05 19:27:22 -0500

    Reorgranized Codebase: Put main function in dedacated file

---
+++ src/finman/main.py
@@ -0,0 +1,38 @@
+import curses # imports curses a barebones highly portable tui library
+from finman.ui.main_menu import MainMenu
+
+def main():
+    screen = curses.initscr() # creates the screen object we will be working with
+    curses.noecho() # stops key presses from being shown on the screen by default
+    curses.cbreak() # get key presses without the user pressing enter
+    screen.keypad(True) # get special values instead of multi-key sequences for certain key presses
+    screen.nodelay(True) # make getchr non-blocking
+    curses.curs_set(0) # disable the cursor
+    curses.start_color() # enable the usage of colors
+    num_rows, num_cols = screen.getmaxyx() 
+    menu_window = curses.newwin(1, 1, 3, 0)
+    title_window = curses.newwin(1, 1, 0, 0)
+    
+    inpt = screen.getch()
+    main_menu = MainMenu(screen)
+    current_scene = main_menu
+    while inpt != curses.KEY_ENTER and inpt != 10 and inpt!= 13:
+
+        scene = None
+        current_scene.full_pass(inpt,scene)
+
+        if scene != None:
+            current_scene = scene
+        # limit the speed of the app
+        curses.napms(100)
+        # check for key presses
+        inpt = screen.getch()
+    
+    # Undoes all the changes we made to the terminal during initialization
+    curses.nocbreak()
+    screen.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+if __name__ == "__main__":
+    main()
--- src/finman/ui/main_menu.py
+++ src/finman/ui/main_menu.py
@@ -2,46 +2,12 @@ import curses # imports curses a barebones highly portable tui library
 from finman.util.menus import build_menu
 from finman.ui.scene import Scene
 
-def main():
-    screen = curses.initscr() # creates the screen object we will be working with
-    curses.noecho() # stops key presses from being shown on the screen by default
-    curses.cbreak() # get key presses without the user pressing enter
-    screen.keypad(True) # get special values instead of multi-key sequences for certain key presses
-    screen.nodelay(True) # make getchr non-blocking
-    curses.curs_set(0) # disable the cursor
-    curses.start_color() # enable the usage of colors
-    num_rows, num_cols = screen.getmaxyx() 
-    menu_window = curses.newwin(1, 1, 3, 0)
-    title_window = curses.newwin(1, 1, 0, 0)
-    
-    inpt = screen.getch()
-    main_menu = MainMenu(screen)
-    current_scene = main_menu
-    while inpt != curses.KEY_ENTER and inpt != 10 and inpt!= 13:
-
-        scene = None
-        current_scene.full_pass(inpt,scene)
-
-        if scene != None:
-            current_scene = scene
-        # limit the speed of the app
-        curses.napms(100)
-        # check for key presses
-        inpt = screen.getch()
-    
-    # Undoes all the changes we made to the terminal during initialization
-    curses.nocbreak()
-    screen.keypad(False)
-    curses.echo()
-    curses.endwin()
-
-
 class MainMenu(Scene):
     def __init__(self,screen):
         super().__init__(screen)
         self.entered = False
         self.selected = 0
-        self.options = ["Overview", "Add Transations", "Modify Budget"]
+        self.options = ["Overview", "Transations", "Budget"]
         self.menu_window = curses.newwin(1, 1, 3, 0)
         self.title_window = curses.newwin(1, 1, 0, 0)
         self.screen = screen
@@ -88,5 +54,3 @@ class MainMenu(Scene):
     def on_exit(self):
         pass
 
-if __name__ == "__main__":
-    main()
